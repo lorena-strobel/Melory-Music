f52f8107f0fcab8b5c7e3232ded43efe3eecb207
Lorena Strobel <lorenastrobel97@gmail.com>
2026-04-07 10:20:28 -0400

    fix: organiza css em stylesheets e conserta erro de exibição em movimentação de estoque

--- app/models/stock_movement.rb
+++ app/models/stock_movement.rb
@@ -1,10 +1,10 @@
 class StockMovement < ApplicationRecord
   belongs_to :item
   delegate :name_item, to: :item, allow_nil: true
-  validates :item_id, presence: { message: "Item deve ser selecionado"}
-  validates :reason, presence: {message: "Motivo deve ser preenchido"}
-  validates :movement_date, presence: {message: "Data e horário devem ser preenchidos"}
-  validates :quantity_stock, presence: true, numericality: {
+  validates :item_id, presence: { message: "Item deve ser selecionado" }
+  validates :movement_date, presence: { message: "Data e horário devem ser preenchidos" }
+  validates :reason, presence: { message: "Motivo deve ser preenchido" }
+  validates :quantity_stock, numericality: {
       only_integer: true,
       greater_than_or_equal_to: 0,
       message: "Quantidade deve ser um número inteiro"
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,5 @@ Rails.application.config.assets.paths << Rails.root.join("node_modules")
 # folder are already added.
 # Rails.application.config.assets.precompile += %w[ admin.js admin.css ]
 Rails.application.config.assets.precompile << "bootstrap.bundle.min.js"
+Rails.application.config.assets.precompile += %w[ sidebar.css ]
+Rails.application.config.assets.precompile += %w[ home.css ]
